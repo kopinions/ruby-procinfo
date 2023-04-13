dbb18bc61b6feb09416462e17685eb77ba18cf97
neo <rising.lambda@gmail.com>
2023-04-13 14:13:50 +0800

    fix: the performance spec fix

--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -1,37 +1,41 @@
 require "minitest/benchmark"
+require 'benchmark'
 require 'posix/spawn'
+require 'procinfo'
 
 describe "Process information retrieval" do
-  bench_performance_constant "System.uname" do
-    System.uname
+  Benchmark.bm  do |x|
+    x.report {System.uname}
   end
 
-  bench_performance_constant "Process.stats" do
-    Process.stats
+  Benchmark.bm  do |x|
+    x.report {Process.stats}
   end
 
-  bench_performance_constant "Process.stats_self" do
-    Process.stats(:self)
+  Benchmark.bm  do |x|
+    x.report {Process.stats(:self)}
   end
 
-  bench_performance_constant "Process.stats_children" do
-    Process.stats(:children)
+  Benchmark.bm  do |x|
+    x.report {Process.stats(:children)}
   end
 
-  bench_performance_constant "Spawn_ps_rss", 0.9 do
-    `ps -o rss= -p #{Process.pid}`.to_i
+  Benchmark.bm do |x|
+    x.report {`ps -o rss= -p #{Process.pid}`.to_i}
   end
 
-  bench_performance_constant "POSIX::Spawn.popen4" do
-    begin
-      pid, stdin, stdout, stderr = POSIX::Spawn.popen4('ps', "-o rss= -p #{Process.pid}")
-      stdin.close
-      rss = stdout.read.to_i
-    rescue => e
+  Benchmark.bm  do |x|
+    x.report {
+      begin
+        pid, stdin, stdout, stderr = POSIX::Spawn.popen4('ps', )
+        stdin.close
+        rss = stdout.read.to_i
+      rescue => e
       # nothing right now.
-    ensure 
-      [stdin, stdout, stderr].each { |fd| fd.close unless fd.closed? }
-      stat = Process::waitpid(pid)
-    end
+      ensure 
+        [stdin, stdout, stderr].each { |fd| fd.close unless fd.closed? }
+        stat = Process::waitpid(pid)
+      end
+    }
   end
 end
